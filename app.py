0ac34a13a21fc307399ddcce1ae73411ffa77107
varun00391 <varunsingh2191@gmail.com>
2025-09-09 17:44:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello Azure App Service!"}
+    return {"message": "🚀 Hello from FastAPI on Azure App Service!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "query": q}
